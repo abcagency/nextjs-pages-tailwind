fbdc475478c7bbea7075a8cc09cc54c983d84163
jcock <jason.cockerham@gmail.com>
2023-05-18 12:36:03 -0400

    Added basic form

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,6 @@
 import Layout from '~/components/layout/layout';
 import PageHead from '~/components/util/page-head';
+import Form from '~/components/modules/form';
 
 const ContactPage = ({ router }) => {
 	return (
@@ -11,6 +12,10 @@ const ContactPage = ({ router }) => {
 
 			<section className="container px-4 py-12 text-center">
 				<h1 className="text-3xl">Contact page</h1>
+			</section>
+
+			<section className="container px-4 py-12">
+				<Form />
 			</section>
 		</Layout>
 	);
