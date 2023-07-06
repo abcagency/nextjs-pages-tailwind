6d86c538c1944ae024df3d065f25ea46c3a24f4b
jcock <jason.cockerham@gmail.com>
2023-07-06 09:10:41 -0400

    Fixed css nesting

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,13 @@
 module.exports = {
 	plugins: {
+		'tailwindcss/nesting': 'postcss-nesting',
 		tailwindcss: {},
 		'postcss-preset-env': {
 			autoprefixer: false,
-			stage: 0
+			stage: 0,
+			features: {
+				'nesting-rules': false
+			}
 		},
 		autoprefixer: {}
 	}
