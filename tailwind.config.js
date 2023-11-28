36c7c5ff64e71a4cc77bc15dca6170f2566f94b3
jcock <jason.cockerham@gmail.com>
2023-11-28 11:07:33 -0500

    Added tailwind-animate

--- tailwind.config.js
+++ tailwind.config.js
@@ -111,6 +111,7 @@ module.exports = {
 	plugins: [
 		require('@tailwindcss/aspect-ratio'),
 		require('@tailwindcss/forms'),
-		require('@tailwindcss/typography')
+		require('@tailwindcss/typography'),
+		require('tailwindcss-animate')
 	]
 };
