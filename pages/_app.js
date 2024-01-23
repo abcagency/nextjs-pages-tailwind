e319304a691cbc787f9ef9616e64a8a8b2469533
jcock <jason.cockerham@gmail.com>
2024-01-23 09:06:33 -0500

    Added GTM next package

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Script from 'next/script';
+import { GoogleTagManager } from '@next/third-parties/google';
 import NextProgress from 'next-progress';
 import { inter } from '~/styles/fonts';
 
@@ -22,14 +22,7 @@ const App = ({ Component, pageProps }) => {
 				}
 			/>
 			{(gtmId && process.env.NODE_ENV === 'production') &&
-				<Script id="google-tag-manager" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `
-					(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-					})(window,document,'script','dataLayer','${site.analytics.gtmId}');
-				` }}>
-				</Script>
+				<GoogleTagManager gtmId={site.analytics.gtmId} />
 			}
 			<div
 				className={`
