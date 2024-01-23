de5c055cbbf1472a4bf250f414c712307bf95237
jcock <jason.cockerham@gmail.com>
2024-01-23 09:47:05 -0500

    Removed commented code

--- hooks/useEventTracker.js
+++ hooks/useEventTracker.js
@@ -1,23 +1,3 @@
-// const isBrowser = typeof window !== 'undefined';
-
-// if (isBrowser) {
-// 	window.dataLayer = window.dataLayer || [];
-// }
-
-// const trackEvent = (category, action, label, value) => {
-// 	if (isBrowser && window.dataLayer) {
-// 		window.dataLayer.push({
-// 			'event': 'eventTracking',
-// 			'category': category,
-// 			'action': action,
-// 			'label': label,
-// 			'value': value
-// 		});
-// 	}
-// };
-
-// export default trackEvent;
-
 import { sendGTMEvent } from '@next/third-parties/google';
 
 const trackEvent = (category, action, label, value) => {
