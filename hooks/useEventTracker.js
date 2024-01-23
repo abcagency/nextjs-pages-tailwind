74bf4ce4567d79f19f2726322949b1b683f8ef77
jcock <jason.cockerham@gmail.com>
2024-01-23 09:44:16 -0500

    Added template specific GTM ID for testing

--- hooks/useEventTracker.js
+++ hooks/useEventTracker.js
@@ -1,19 +1,33 @@
-const isBrowser = typeof window !== 'undefined';
+// const isBrowser = typeof window !== 'undefined';
 
-if (isBrowser) {
-	window.dataLayer = window.dataLayer || [];
-}
+// if (isBrowser) {
+// 	window.dataLayer = window.dataLayer || [];
+// }
+
+// const trackEvent = (category, action, label, value) => {
+// 	if (isBrowser && window.dataLayer) {
+// 		window.dataLayer.push({
+// 			'event': 'eventTracking',
+// 			'category': category,
+// 			'action': action,
+// 			'label': label,
+// 			'value': value
+// 		});
+// 	}
+// };
+
+// export default trackEvent;
+
+import { sendGTMEvent } from '@next/third-parties/google';
 
 const trackEvent = (category, action, label, value) => {
-	if (isBrowser && window.dataLayer) {
-		window.dataLayer.push({
-			'event': 'eventTracking',
-			'category': category,
-			'action': action,
-			'label': label,
-			'value': value
-		});
-	}
+	sendGTMEvent({
+		'event': 'eventTracking',
+		'category': category,
+		'action': action,
+		'label': label,
+		'value': value
+	});
 };
 
 export default trackEvent;
