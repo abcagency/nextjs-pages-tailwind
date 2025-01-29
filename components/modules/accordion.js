271035605f5aeb951a12d3f4108ec834de4fbb9b
jcock <jason.cockerham@gmail.com>
2025-01-29 09:39:09 -0500

    Broke out styles for accordion

--- components/modules/accordion.js
+++ components/modules/accordion.js
@@ -8,6 +8,7 @@ import trackEvent from '~/hooks/useEventTracker';
 
 const Accordion = ({
 	className,
+	variant = 'accordion-default',
 	type = 'single',
 	defaultValue,
 	collapsible = true,
@@ -18,7 +19,11 @@ const Accordion = ({
 			type={type}
 			defaultValue={defaultValue ?? null}
 			collapsible={collapsible}
-			className={className ?? ''}
+			className={twMerge`
+				accordion
+				${variant}
+				${className ?? ''}
+			`}
 			onValueChange={value => trackEvent('Engagement', 'Open Accordion', value)}
 		>
 			{children}
@@ -27,12 +32,15 @@ const Accordion = ({
 };
 
 export const AccordionItem = forwardRef(
-	({ id, children, className, ...props }, forwardedRef) => {
+	({ id, children, className, ...props }, ref) => {
 		return (
 			<RadixAccordion.Item
-				ref={forwardedRef}
+				ref={ref}
 				value={id}
-				className={className ?? ''}
+				className={twMerge`
+					accordion-item
+					${className ?? ''}
+				`}
 				{...props}
 			>
 				{children}
@@ -42,21 +50,21 @@ export const AccordionItem = forwardRef(
 );
 
 export const AccordionTrigger = forwardRef(
-	({ children, className, ...props }, forwardedRef) => (
+	({ children, icon = 'mdi:chevron-down', className, ...props }, ref) => (
 		<RadixAccordion.Header asChild>
 			<RadixAccordion.Trigger
-				ref={forwardedRef}
-				className={twMerge(
-					'group flex justify-between w-full p-4 font-bold text-left text-gray-700 focus:outline-hidden focus-visible:ring-3 focus-visible:ring-purple-500 focus-visible:ring-opacity-75 hover:bg-gray-200 focus:bg-gray-200 transition-colors border-b border-white data-[state=closed]:bg-gray-100 data-[state=open]:bg-gray-200',
-					className ?? ''
-				)}
+				ref={ref}
+				className={twMerge`
+					accordion-trigger group
+					${className ?? ''}
+				`}
 				{...props}
 			>
 				{children}
 				<Icon
-					icon="mdi:chevron-down"
-					size="w-4 h-4"
-					className="transition-transform transform! translate-y-1! group-data-[state=open]:rotate-180!"
+					icon={icon}
+					size="size-5"
+					className="trigger-icon"
 					aria-hidden="true"
 				/>
 			</RadixAccordion.Trigger>
@@ -65,20 +73,20 @@ export const AccordionTrigger = forwardRef(
 );
 
 export const AccordionContent = forwardRef(
-	({ children, className, bodyClassName, ...props }, forwardedRef) => (
+	({ children, className, bodyClassName, ...props }, ref) => (
 		<RadixAccordion.Content
-			ref={forwardedRef}
-			className={twMerge(
-				'data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp overflow-hidden',
-				className ?? ''
-			)}
+			ref={ref}
+			className={twMerge`
+				accordion-content
+				${className ?? ''}
+			`}
 			{...props}
 		>
 			<div
-				className={twMerge(
-					'px-4 py-2 border border-gray-200',
-					bodyClassName ?? ''
-				)}
+				className={twMerge`
+					content-body
+					${bodyClassName ?? ''}
+				`}
 			>
 				{children}
 			</div>
