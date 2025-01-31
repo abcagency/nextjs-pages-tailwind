2ad2b7c39194b58c6dc3b59321fed7012151e45e
jcock <jason.cockerham@gmail.com>
2025-01-31 09:19:58 -0500

    Fixed jumbotron on home page

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ const Home = () => {
 		<Layout>
 			<PageHead title="" />
 
-			<Jumbotron className="bg-transparent text-gray-800!">
+			<Jumbotron className="bg-transparent! text-gray-850!">
 				<div className="flex flex-col items-center justify-between pt-32 pb-40">
 					<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-transparent before:to-blue-700 before:opacity-20 before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-900 after:via-[#0141ff] after:opacity-60 after:blur-2xl after:content-[''] lg:before:h-[360px] before:bg-linear-to-br">
 						<Next className="relative w-44" aria-hidden="true" role="img" />
