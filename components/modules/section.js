5af224be633458a23234a494265046f8e58e98d0
jcock <jason.cockerham@gmail.com>
2025-02-03 12:12:14 -0500

    Moved section class to css

--- components/modules/section.js
+++ components/modules/section.js
@@ -27,7 +27,7 @@ const Section = ({ children, className, ...props }) => {
 			threshold={[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]}
 			onChange={onChange}
 			className={twMerge`
-				section scroll-mt-20
+				section
 				${className ?? ''}
 			`}
 			{...props}
