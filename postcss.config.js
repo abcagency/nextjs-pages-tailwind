f488e0fa8683dbfce72d1dbbae80e9e159aa7a4e
jcock <jason.cockerham@gmail.com>
2026-01-06 10:09:14 -0500

    Swapped tailwindcss-animate for tw-animate-css

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,5 @@
 module.exports = {
 	plugins: {
-		'@tailwindcss/postcss': {},
-		'postcss-preset-env': {
-			autoprefixer: false,
-			stage: 0,
-			features: {
-				'nesting-rules': false
-			}
-		},
+		'@tailwindcss/postcss': {}
 	}
 };
